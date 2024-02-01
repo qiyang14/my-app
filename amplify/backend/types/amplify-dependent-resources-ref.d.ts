2c76dc4eb8d43800bed33d4437cb777339c3b847
qiyang14 <qiyangsun1@gmail.com>
2024-02-01 18:57:11 +0800

    backend graphQL amazon

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myapptradeprediction": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
